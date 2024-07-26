ab157ad5c0f6b570557630d155c6fecff6d902ae
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-07-25 22:39:41 -0300

    Animal color information added.

--- src/main/java/com/liro/animals/dto/responses/AnimalResponse.java
+++ src/main/java/com/liro/animals/dto/responses/AnimalResponse.java
@@ -3,9 +3,11 @@ package com.liro.animals.dto.responses;
 
 import com.liro.animals.dto.AnimalDTO;
 import com.liro.animals.dto.AnimalsSharedClientProfilesDTO;
+import com.liro.animals.model.dbentities.AnimalColor;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
@@ -29,4 +31,5 @@ public class AnimalResponse extends AnimalDTO {
     private Set<AnimalsSharedClientProfilesDTO> sharedWith;
     private boolean disabled;
     private boolean death;
+    private Set<AnimalColor> colors;
 }
