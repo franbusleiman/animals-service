1e841c567ccf58cd4028a8dfc1577396da944164
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-08-24 12:26:30 -0300

    Implementing the changeShareStateAnimal service.

--- src/main/java/com/liro/animals/model/dbentities/idclasses/AnimalsSharedUserIdIdClass.java
+++ src/main/java/com/liro/animals/model/dbentities/idclasses/AnimalsSharedUserIdIdClass.java
@@ -1,8 +1,11 @@
 package com.liro.animals.model.dbentities.idclasses;
 
 
+import lombok.EqualsAndHashCode;
+
 import java.io.Serializable;
 
+@EqualsAndHashCode
 public class AnimalsSharedUserIdIdClass implements Serializable {
     private Long animal;
     private Long userId;
