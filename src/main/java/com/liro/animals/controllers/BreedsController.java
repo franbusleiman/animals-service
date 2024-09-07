50f4e5f0fc24257b42309f25060d03abe37b81b4
franbusleiman <franbusleiman@gmail.com>
2024-09-07 19:03:53 -0300

    c

--- src/main/java/com/liro/animals/controllers/BreedsController.java
+++ src/main/java/com/liro/animals/controllers/BreedsController.java
@@ -12,14 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -74,7 +67,7 @@ public class BreedsController {
     }
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<ApiResponse> createBreed(@Valid @RequestBody BreedDTO breedDto) {
+    public ResponseEntity<ApiResponse> createBreed(@Valid @RequestBody BreedDTO breedDto,  @RequestHeader(name = "Authorization", required = false) String token) {
         BreedResponse breedResponse = breedService.createBreed(breedDto);
 
         URI location = ServletUriComponentsBuilder
