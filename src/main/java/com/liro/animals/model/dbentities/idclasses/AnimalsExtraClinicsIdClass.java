5f28d321e990f9b93f5652ab8a9d0ccf8439c8a5
franbusleiman <franbusleiman@gmail.com>
2024-10-19 14:51:28 -0300

    c

--- src/main/java/com/liro/animals/model/dbentities/idclasses/AnimalsExtraClinicsIdClass.java
+++ src/main/java/com/liro/animals/model/dbentities/idclasses/AnimalsExtraClinicsIdClass.java
@@ -4,5 +4,5 @@ import java.io.Serializable;
 
 public class AnimalsExtraClinicsIdClass implements Serializable {
     private Long animal;
-    private Long vetUserId;
+    private Long clinicId;
 }
