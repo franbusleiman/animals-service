8d7a861c116b4f04cfa936f021e32547a3a9caaa
franbusleiman <franbusleiman@gmail.com>
2024-11-09 19:21:14 -0300

    c

--- src/main/java/com/liro/animals/config/FeignClinicClientClient.java
+++ src/main/java/com/liro/animals/config/FeignClinicClientClient.java
@@ -1,5 +1,6 @@
 package com.liro.animals.config;
 
+import com.liro.animals.dto.ApiResponse;
 import com.liro.animals.dto.UserResponseDTO;
 import com.liro.animals.dto.requests.ClinicClientDTO;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -12,7 +13,7 @@ import java.util.List;
 @FeignClient(name = "clinics-service")
 public interface FeignClinicClientClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/clinicClients")
-    ResponseEntity<Void> createClinicClient(@RequestHeader(name = "Authorization", required = false) String token,
-                                            @RequestBody ClinicClientDTO clinicClientDto);
+    @PostMapping(value = "/clinicClients")
+    ResponseEntity<ApiResponse> createClinicClient(@RequestHeader(name = "Authorization", required = false) String token,
+                                                   @RequestBody ClinicClientDTO clinicClientDto);
 }
