afa79ed39222fdab811e2b43aa8bda259a5dc5e1
franbusleiman <franbusleiman@gmail.com>
2024-11-27 23:51:59 -0300

    c

--- src/main/java/com/liro/animals/service/impl/AnimalServiceImpl.java
+++ src/main/java/com/liro/animals/service/impl/AnimalServiceImpl.java
@@ -25,6 +25,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -186,6 +188,9 @@ public class AnimalServiceImpl implements AnimalService {
 
                 Record record = new Record();
                 record.setRecordType(recordType);
+                record.setDataString(animalRequest.getPeso());
+                record.setValidData(false);
+                record.setDate(LocalDateTime.now());
 
                 if (recordType.getRecords() == null) recordType.setRecords(new HashSet<>());
                 recordType.getRecords().add(record);
