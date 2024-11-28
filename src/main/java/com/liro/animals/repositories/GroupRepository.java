1b5b7b7bd7232ca1fe8e0f9e3b8545897f6ff50b
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-11-28 18:40:44 -0300

    Deleting group class

--- src/main/java/com/liro/animals/repositories/GroupRepository.java
+++ src/main/java/com/liro/animals/repositories/GroupRepository.java
@@ -1,6 +1,5 @@
 package com.liro.animals.repositories;
 
-import com.liro.animals.model.dbentities.Group;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
